900d97014e83d6fffb0398e848d8944a0d69448e
Deniskiplangat <denisruto3561@gmail.com>
2026-04-14 12:07:39 +0300

    Updated something

--- api/server.js
+++ api/server.js
@@ -1,29 +1,71 @@
-import pool from './db.js';
+// import pool from './db.js';
 
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ message: 'Method not allowed' });
+//   }
 
-  let conn;
+//   let conn;
 
-  try {
-    const { username, email, password } = req.body;
+//   try {
+//     const { username, email, password } = req.body;
 
-    conn = await pool.getConnection();
+//     conn = await pool.getConnection();
 
-    await conn.query(
+//     await conn.query(
+//       "INSERT INTO users(username, email, password) VALUES (?, ?, ?)",
+//       [username, email, password]
+//     );
+
+//     res.status(200).json({ message: "User added successfully" });
+
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Error adding user" });
+
+//   } finally {
+//     if (conn) conn.release();
+//   }
+// }
+
+
+const express = require('express')
+const cors = require('cors');
+const pool = require('./db')
+
+
+
+const app = express()
+
+//Enable CORS
+app.use(cors());
+
+//middleware to read the json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+//route to handle form submission
+app.post('/add-user',async (req,res) =>{
+    let conn;
+    try {
+        const { username, email, password } = req.body;
+        conn = await pool.getConnection();
+        await conn.query(
       "INSERT INTO users(username, email, password) VALUES (?, ?, ?)",
       [username, email, password]
     );
 
-    res.status(200).json({ message: "User added successfully" });
+    res.send("User added successfully");
+       
+   
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error adding user");
+    }finally{
+        if (conn) conn.release();
+    }
+})
 
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Error adding user" });
-
-  } finally {
-    if (conn) conn.release();
-  }
-}
+app.listen("3001",()=>{
+    console.log('server is running..')
+})
